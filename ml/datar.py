58efbb6d9072d73f24fd000441196a2e30d26880
Sanjay Dwivedi <skdwivedi@comcast.net>
2018-03-13 20:31:22 -0700

    graph adjustments

--- ml/datar.py
+++ ml/datar.py
@@ -1,5 +1,6 @@
 import csv
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import math
 import sys
@@ -59,7 +60,7 @@ dim = data.shape;
 #print(dim[0]);
 #print(dim[1]);
 
-plt.scatter(data[:,0],data[:,1]);
+plt.scatter(data[:,0],data[:,1], marker='+');
 plt.xlabel("Population of city in 10,000.");
 plt.ylabel("Profit in $10,000.00")
 #plt.show();
@@ -71,7 +72,7 @@ y = y.reshape(dim[0],1);
 columVec = np.ones((dim[0],1),dtype=np.float_);
 #columVec = columVec.reshape(dim[0],1);
 X = data[:,0];
-X=X.reshape(dim[0],1);
+X_axis = X=X.reshape(dim[0],1);
 X=np.append(columVec,X,axis=1);
 #print(X);
 
@@ -97,7 +98,9 @@ printf("Theta found by gradient descent:\n");
 printf("[%.4f, %.4f]\n", theta[0],theta[1]);
 printf("Expected theta values (approx)\n");
 printf(" -3.6303\n  1.1664\n\n");
-
+myval = np.dot(X,theta);
+print(myval.shape);
+plt.scatter(X_axis,myval, marker='.', c='r');
 # Predict values for population sizes of 35,000 and 70,000
 predict1 = theta[0] + 3.5*theta[1];
 printf("For population = 35,000, we predict a profit of %f\n",predict1*10000);
@@ -113,7 +116,20 @@ for i  in range (0,len(theta0_vals)):
 	for j in range (0,len(theta1_vals)):
 		t = np.array([[theta0_vals[i]], [theta1_vals[j]]]);
 		J_vals[i,j] = computeCost(X, y, t);
+
+J_vals = J_vals.transpose();
 fig=plt.figure();
 ax=fig.gca(projection='3d');
-ax.plot_surface(theta0_vals, theta1_vals,J_vals,rstride=8, cstride=8, alpha=0.3);
+ax.plot_surface(theta0_vals, theta1_vals,J_vals,cmap=cm.Spectral,rstride=4, cstride=4,linewidth=1, antialiased=False);
+plt.xlabel('\theta_0'); plt.ylabel('\theta_1'); plt.title('Cost function')
+
+# draw countours
+# Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
+plt.figure();
+CS = plt.contour(theta0_vals,theta1_vals,J_vals, np.logspace(-2,3,num=20));
+
+#contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+plt.xlabel('\theta_0'); plt.ylabel('\theta_1');
+plt.scatter(theta[0],theta[1], marker='+', c='r');
+
 plt.show();
